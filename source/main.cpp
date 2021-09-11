669569245778bc9f5976bc05f245be3b67d86563
jdotaz <juan.obando.zuniga@ucsp.edu.pe>
2021-09-11 01:39:24 -0500

    Commit de main.cpp, git ignore and CMakeList

---
+++ source/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+// Declaración de funciones
+double euclidean_distance(vector<int>, vector<int>);
+
+// Variables globales
+int dimensions = 2;
+
+int main() {
+
+
+    // Inicialización de las variables y la uniform int distribution
+
+    random_device rd;       // Semilla del aleatorio
+    mt19937 gen(rd());      // Semilla del aleatorio
+    int rand_min = 1;       // Valor mínimo del aleatorio
+    int rand_max = 1000;    // Valor máximo del aleatorio
+    int n_points = 20000;   // Número de puntos generados
+    uniform_int_distribution<> distrib(rand_min, rand_max); // Generador de aleatorios
+    vector<vector<int>> mat_points; // Vector de los puntos generados
+
+
+    // Generador de los puntos aleatorios con n dimensiones
+
+    for (int n = 0; n < n_points; ++n) {
+        vector<int> point;
+        for (int i = 0; i < dimensions; ++i) {
+            point.push_back(distrib(gen));
+        }
+        mat_points.push_back(point);
+    }
+
+
+    // Calcular distancias euclidianas y la distancia máxima y mínima
+
+    vector<int> selector = mat_points[n_points / 2]; // Punto seleccionado del medio para calcular sus distancias con los demás
+    vector<double> distances;   // Vector de distancias
+    double min;                 // Elemento mínimo
+    double max;                 // Elemento máximo
+    for (int i = 0; i < mat_points.size(); ++i) {
+        double distance = euclidean_distance(selector, mat_points[i]); // Distancia Euclidiana
+        distances.push_back(distance);
+        if (i == 0) {
+            min = distance;
+            max = distance;
+        }
+        if (distance > max) {
+            max = distance;
+        } else {
+            if (distance < min and distance != 0) {
+                min = distance;
+            }
+        }
+    }
+
+
+    // Conteo de frecuencias
+
+    int frec[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    for (double distance: distances) {
+        double percentage = (distance - min) / (max - min);
+        frec[(int) (percentage * 10)]++;
+    }
+
+    // Imprimir resultado
+
+    for (int i = 0; i < 11; ++i) {
+        cout << (double) i / 10 << "\t" << frec[i] << endl;
+    }
+}
+
+//Función distancia Euclidiana
+double euclidean_distance(vector<int> selector, vector<int> temp) {
+    double distance = 0;
+    for (int j = 0; j < dimensions; ++j) {
+        distance += (pow(selector[j] - temp[j], 2));
+    }
+    return pow(distance, 0.5);
+}
